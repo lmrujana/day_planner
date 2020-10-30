06353fc1295cb47533b99a1f84c2b17fdc9a5ce9
Luis Rujana <lmrujanass@gmail.com>
2020-10-29 23:13:24 -0400

    Added the current time checking function that changes the color of the time blocks

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 //This saves "right now" in a variable, from which we can obtain useful information
-var now = moment();
+var now = moment().subtract(11, 'hours');
 
 //Today's date || Top of the page
-$('#currentDay').text(now.format('dddd, MMMM Do YYYY, kk:mm'));
+$('#currentDay').text(now.format('dddd, MMMM Do YYYY, kk:mm a'));
+
+//Changing the color of the text area depending on the current time.
+$('.text').each(function(index){
+    var hourData = $(this).attr('data-time')
+    var hour = moment().startOf('day').add(hourData, 'h');
+    var timeDifference = hour.diff(now, 'hours', true);
+    if(timeDifference < 0 && timeDifference > -1){
+        $(this).addClass('present');
+    } else if(timeDifference > 0){
+        $(this).addClass('future');
+    }else if(timeDifference < -1){
+        $(this).addClass('past');
+    }
+    console.log(index + ": " + timeDifference);
+})
+
 
